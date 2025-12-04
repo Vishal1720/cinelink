bbf73c1442d41b2c9ec34d71fc5cff4d88c57c81
Vishal <vishal198shetty@gmail.com>
2025-12-04 21:52:55 +0530

    fix

--- src/supabase.js
+++ src/supabase.js
@@ -1,7 +1,7 @@
 // supabaseClient.js
 import { createClient } from '@supabase/supabase-js'
 
-const SUPABASE_URL = 'https://wiggitkoxqislzddubuk.supabase.co'
-const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndpZ2dpdGtveHFpc2x6ZGR1YnVrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjMwMjQxNDEsImV4cCI6MjA3ODYwMDE0MX0.qWSSpTEFaocZVKr5bHgjxaeBx25lp8V45jLWOdl4ANo'
+const SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL;
+const SUPABASE_ANON_KEY = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY)
